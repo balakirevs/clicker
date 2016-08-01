e8f7ab8bf0d97acceecfaa66d5a4c3ea4d8298a3
Aleksandr Balakiriev <balakirevs@i.ua>
2016-08-01 14:33:13 +0200

    change selenium address for cucumber protractor conf

--- test/protractor-cucumber.conf.js
+++ test/protractor-cucumber.conf.js
@@ -8,7 +8,7 @@ exports.config = {
     format: "pretty"
   },
 
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  seleniumAddress: 'http://localhost:4445/wd/hub',
   specs: ['../features/*.feature'],
 
   multiCapabilities: [{
